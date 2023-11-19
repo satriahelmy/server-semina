00fb70cea9017245315ddda48d157984167c866f
Helmy Satria Martha Putra <helmy.satria@pln.co.id>
2023-11-19 21:20:17 +0700

    router categories

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
+
+// router
+const categoriesRouter = require('./app/api/v1/categories/router');
+
+const v1 = '/api/v1/cms'
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -11,10 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res) =>{
+app.get('/', (req, res) =>{
     res.status(200).json({
         message : "Welcome to API Semina" 
     })
 });
+
+app.use(v1, categoriesRouter);
 
 module.exports = app;
